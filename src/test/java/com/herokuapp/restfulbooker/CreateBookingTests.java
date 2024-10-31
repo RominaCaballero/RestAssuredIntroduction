0a8f39edc525590537e7a7e12779a9a64d3681a0
Romina Caballero <rominacaballero8@gmail.com>
2024-10-31 06:45:02 -0300

    Added Serialization

--- src/test/java/com/herokuapp/restfulbooker/CreateBookingTests.java
+++ src/test/java/com/herokuapp/restfulbooker/CreateBookingTests.java
@@ -47,41 +47,25 @@ public class CreateBookingTests extends BaseTest{
 
     @Test
     public void createBookingWithPOJOTest() {
-        // Create body using POJOs.
-        Bookingdates bookingdates = new Bookingdates("2020-03-25","2020-03-27");
-        Booking booking = new Booking("Olga","Shyshkin",200,false,bookingdates,"Baby crib");
+        // Create body using POJOs
+        Bookingdates bookingdates = new Bookingdates("2020-03-25", "2020-03-27");
+        Booking booking = new Booking("Olga", "Shyshkin", 200, false, bookingdates, "Baby crib");
 
         // Get response
-        Response response = RestAssured.given(spec).contentType(ContentType.JSON).
-                body(booking).post("/booking");
-
+        Response response = RestAssured.given(spec).contentType(ContentType.JSON).body(booking)
+                .post("/booking");
         response.print();
+        Bookingid bookingid = response.as(Bookingid.class);
+
         // Verifications
-        // Verify the response is 200
-        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200");
+        // Verify response 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
 
-        // Verify all fields
-        SoftAssert softAssert = new SoftAssert();
-        String actualFirstName = response.jsonPath().getString("booking.firstname");
-        softAssert.assertEquals(actualFirstName, "Olga", "First name should be Mina");
+        System.out.println("Request booking : " + booking.toString());
+        System.out.println("Response booking: " + bookingid.getBooking().toString());
 
-        String actualLastName = response.jsonPath().getString("booking.lastname");
-        softAssert.assertEquals(actualLastName, "Shyshkin", "Last name should be Cebarolla");
-
-        int price = response.jsonPath().getInt("booking.totalprice");
-        softAssert.assertEquals(price, 200, "Price should be 200");
-
-        boolean depositPaid = response.jsonPath().getBoolean("booking.depositpaid");
-        softAssert.assertFalse(depositPaid, "Deposit paid should be False");
-
-
-        String actualCheckin = response.jsonPath().getString("booking.bookingdates.checkin");
-        softAssert.assertEquals(actualCheckin, "2020-03-25", "Checkin is not expected");
-
-        String actualCheckout = response.jsonPath().getString("booking.bookingdates.checkout");
-        softAssert.assertEquals(actualCheckout, "2020-03-27", "Checkout is not expected");
-
-        softAssert.assertAll();
+        // Verify All fields
+        Assert.assertEquals(bookingid.getBooking().toString(), booking.toString());
     }
 
 }
