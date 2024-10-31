0d01ef81d8d453385b9763e4fd9954773c0dfdb1
Romina Caballero <rominacaballero8@gmail.com>
2024-10-31 05:02:40 -0300

    Added Query parameters

--- src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
+++ src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
@@ -23,4 +23,21 @@ public class GetBookingIdsTests extends BaseTest{
         Assert.assertFalse(bookingIds.isEmpty(), "BookingIds should not be empty");
     }
 
+    @Test
+    public void getBookingIdsWithFilterTest() {
+        //Add query parameter to spec
+        spec.queryParam("firstname","Susan");
+
+        // Get response with booking ids
+        Response response = RestAssured.given(spec).get("/booking");
+        response.print();
+
+        // Verify the response is 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200 but it is not");
+
+        // Verify there is at least 1 booking id in the response
+        List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+        Assert.assertFalse(bookingIds.isEmpty(), "BookingIds should not be empty");
+    }
+
 }
